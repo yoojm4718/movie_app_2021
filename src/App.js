a81b82c0c171f087ad0934a487bb11327fc7622e
yoojm4718 <yoojm4718@outlook.com>
2021-08-26 21:59:49 +0900

    Using Axios to Fetch Movie Data and Render it

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-  add = () => {
-    // this is just a JS syntax (Class Method Declaration)
-    // this.state.count += 1; -> it will not show on screen
-    // this.setState({ count: this.state.count + 1 }); -> this will show on screen because react will execute render(), too
-    // but using this.state.count (using external state) can have performance issue.
-    // so we use this
-    this.setState((current) => ({ count: current.count + 1 }));
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
-  minus = () => {
-    // this.state.count -= 1; -> not show on screen
-    // this.setState({ count: this.state.count - 1 }); -> performance issue
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
+  componentDidMount() {
+    this.getMovies();
+  }
   render() {
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>Count: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
